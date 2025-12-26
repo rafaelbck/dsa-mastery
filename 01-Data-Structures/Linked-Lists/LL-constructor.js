e36bab621addcc2eaea93949ee420f6cccd32351
Rafael Becker <rafaelbck@protonmail.com>
2025-12-26 06:02:07 -0300

    new methods for LL: push, pop, unshift, shift, get, set

--- 01-Data-Structures/Linked-Lists/LL-constructor.js
+++ 01-Data-Structures/Linked-Lists/LL-constructor.js
@@ -14,9 +14,87 @@ class LinkedList {
     }
 
     push(value){
-      
+        const newNode = new Node(value)
+        if(!this.head){
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            this.tail.next = newNode
+            this.tail = newNode
+        }
+        this.length++
+        return this
     }
+
+    pop(){
+        if(!this.head) return undefined
+
+        let temp = this.head;
+        let pre = this.head;
+        while(temp.next){
+            pre = temp
+            temp = temp.next
+        }
+        this.tail = pre;
+        this.tail.next = null;
+        this.length--
+        if(this.length === 0){
+            this.head = null;
+            this.tail = null
+        }
+        return temp
+
+    }
+
+    unshift(value){
+        const newNode = new Node(value)
+
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            newNode.next = this.head
+            this.head = newNode
+        }
+        this.length++
+        return this
+    }
+
+    shift(){
+        if(!this.head) return undefined
+        let temp = this.head;
+        this.head = this.head.next;
+        temp.next = null;
+        this.length--
+        if(this.length === 0){
+            this.tail = null;
+        }
+        return temp
+    }
+
+    get(index){
+        if(index < 0 || index >= this.length) return undefined
+        temp = this.head
+        for(i=0; i < index; i++){
+            temp = temp.next
+        }
+        return temp
+    }
+
+    set(index, value){
+        let temp = this.get(index)
+        if(temp){
+            temp.value = value
+            return true
+        } else {
+            return false
+        }
+    }
+
 };
 
-let myLinkedList = new LinkedList(4)
+let myLinkedList = new LinkedList(2)
+myLinkedList.push(1);
+myLinkedList.shift()
+myLinkedList.shift()
 console.log(myLinkedList)
