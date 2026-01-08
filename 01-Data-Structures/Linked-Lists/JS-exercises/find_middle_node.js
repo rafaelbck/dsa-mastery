3e9b76a01a12d821805c2101f63477b82d0721ef
Rafael Becker <rafaelbck@protonmail.com>
2026-01-08 06:00:54 -0300

    first two LL exercises: find middle node and has loop

---
+++ 01-Data-Structures/Linked-Lists/JS-exercises/find_middle_node.js
@@ -0,0 +1,54 @@
+/* 
+    Edge cases: 
+        - No length attribute;
+        - Cannot calculate the length
+        - Only loop through it once
+*/
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+ 
+class LinkedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  findMiddleNode() {
+    let fast = this.head;
+    let slow = this.head;
+    if (!fast) return null;
+
+    while (fast && fast.next) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+    return slow;
+  }
+}
+
+let myLinkedList = new LinkedList(1);
+myLinkedList.push(2);
+myLinkedList.push(3);
+myLinkedList.push(4);
+myLinkedList.push(5);
+
+console.log(myLinkedList.findMiddleNode())
---
+++ 01-Data-Structures/Linked-Lists/JS-exercises/has_loop.js
@@ -0,0 +1,57 @@
+/*
+    Edge cases: 
+        - No length attribute;
+        - Cannot calculate the length
+        - Only loop through it once
+        - Verify if the linked list has a loop
+*/
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  hasLoop() {
+    let fast = this.head;
+    let slow = this.head;
+
+    while (fast && fast.next) {
+      slow.next;
+      fast.next.next;
+      if (fast === slow){
+          return true;
+      } 
+    }
+    return false;
+  }
+}
+
+let myLinkedList = new LinkedList(1);
+myLinkedList.push(2);
+myLinkedList.push(3);
+myLinkedList.push(4);
+myLinkedList.push(5);
+
+console.log(myLinkedList.findMiddleNode());
