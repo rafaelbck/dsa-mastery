210e255de9d9ff91cc158c247bd5b99fa31e17c9
Rafael Becker <rafaelbck@protonmail.com>
2026-01-09 13:27:14 -0300

    find kth node from end exercise complete

--- 01-Data-Structures/Linked-Lists/JS-exercises/find_kth_node_from_end.js
+++ 01-Data-Structures/Linked-Lists/JS-exercises/find_kth_node_from_end.js
@@ -3,17 +3,17 @@
     Edge cases: 
         - No length attribute;
         - Cannot calculate the length
-        - if k > length return null
+        - if k > length return null 
         - use fast & slow
 */
 
 class Node {
-    constructor(value){
-        this.value = value;
-        this.next = null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
- 
+
 class LinkedList {
   constructor(value) {
     const newNode = new Node(value);
@@ -35,10 +35,24 @@ class LinkedList {
     return this;
   }
 
-  findKthNode(k){
+  findKthFromEnd(k) {
+    let slow = this.head;
+    let fast = this.head;
 
+    for (let i = 0; i < k; ++i) {
+      if (fast === null) {
+        return null;
+      }
+      fast = fast.next;
+    }
+
+    while (fast !== null) {
+      slow = slow.next;
+      fast = fast.next;
+    }
+
+    return slow;
   }
-
 }
 
 let myLinkedList = new LinkedList(1);
@@ -47,4 +61,4 @@ myLinkedList.push(3);
 myLinkedList.push(4);
 myLinkedList.push(5);
 
-console.log(myLinkedList.findMiddleNode())
+console.log(myLinkedList.findMiddleNode());
