f95573d74cadc8dee70a66ea9d41f22d0301b5a1
Rafael Becker <rafaelbck@protonmail.com>
2026-01-16 06:10:59 -0300

    starting DLL constructor

--- 02-Algorithms/DLL-constructor.js
+++ 02-Algorithms/DLL-constructor.js
@@ -13,6 +13,53 @@ class DoublyLinkedList {
         this.tail = this.head
         this.length = 1
     }
+
+    // O(1)
+    push(value){
+        const newNode = new Node(value);
+        if(!this.head){
+            this.head = newNode;
+            this.head = newNode
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode
+        }
+        this.length++
+        return this
+    }
+
+    // O(1)
+    pop(){
+        if(this.length === 0) return undefined;
+        let temp = this.tail      
+        if (this.length === 1){
+            this.head = null
+            this.tail = null
+        } else {
+        this.tail = this.tail.prev
+        this.tail.next = null
+        temp.prev = null
+        }
+        this.length--
+        return temp
+    }
+
+    unshift(value){
+        const newNode = new Node(value)
+        if (!this.length === 0){
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            newNode.next = this.head
+            this.head.prev = newNode
+            this.head = newNode
+        }
+        this.length++
+        return this
+    }
 }
 let myDLL = new DoublyLinkedList(7)
-console.log(myDLL)
+myDLL.push(1)
+myDLL.push(3)
+console.log(JSON.stringify(myDLL))
